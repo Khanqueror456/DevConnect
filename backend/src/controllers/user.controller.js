f11603ab88b719c8dd463dc060cdbd2828394893
Khanqueror456 <unrealengine456@gmail.com>
2026-03-04 00:15:35 +0530

    Add Accept Request Feature

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -7,7 +7,7 @@ export const sendConnectionRequest = async (req, res) => {
         const senderId = req.user._id;
         const { userId } = req.params;
 
-        if (senderId.toString() === userId)
+        if (senderId.toString() === userId.toString())
         {
             return res.status(400).json({ message : "Cannot connect with yourself"});
         }
@@ -36,4 +36,43 @@ export const sendConnectionRequest = async (req, res) => {
     } catch (error) {
         res.status(500).json({ message : error.message});
     }
-}
+};
+
+export const acceptConnectionRequest = async (req, res) => {
+    try {
+
+        const currentUserId = req.user._id; // the one accepting
+        const { userId } = req.params; // the one who sent request
+
+        const currentUser = await User.findById(currentUserId);
+        const senderUser = await User.findById(userId);
+
+        if (!senderUser)
+        {
+            return res.status(404).json({ message : "User not found"});
+        }
+
+        // Check if request exists
+        if (!currentUser.connectionRequests.includes(userId)) {
+            return res.status(400).json({ message : "No connection request found"});
+        }
+
+        // Remove from requests
+        currentUser.connectionRequests = currentUser.connectionRequests.filter(
+            (id) => id.toString() !== userId.toString()
+        );
+
+        // Add to connections (both sides)
+        currentUser.connections.push(userId);
+        senderUser.connections.push(currentUserId);
+
+        await currentUser.save();
+        await senderUser.save();
+
+        res.json({ message : "Connection request accepted"});
+
+
+    } catch (error) {
+        res.status(500).json({ message : error.message});
+    }
+};
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { protect } from "../middleware/auth.middleware.js";
-import { sendConnectionRequest } from "../controllers/user.controller.js";
+import { sendConnectionRequest, acceptConnectionRequest } from "../controllers/user.controller.js";
 
 
 const router = express.Router();
 
 router.post("/connect/:userId", protect, sendConnectionRequest);
+router.post("/accept/:userId", protect, acceptConnectionRequest);
 
 export default router;
